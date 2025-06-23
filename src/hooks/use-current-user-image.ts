1aca2b5d3cf2a9523011cde89e9931d0ea915e94
Phạm Hoàng Minh Anh <115133340+MinhAnh2610@users.noreply.github.com>
2025-06-23 10:15:12 +0700

    fix: linter errors

--- src/hooks/use-current-user-image.ts
+++ src/hooks/use-current-user-image.ts
@@ -1,28 +1,15 @@
-import { createClerkSupabaseClient } from '@/utils/supabaseClient';
 import { useEffect, useState } from 'react';
-import { useSession } from '@clerk/clerk-react'; // Assuming @clerk/clerk-react, adjust if using a different Clerk package e.g. @clerk/nextjs
+import { useUser } from '@clerk/clerk-react';
 
 export const useCurrentUserImage = () => {
-  const [image, setImage] = useState<string | null>(null)
-
-  const { session } = useSession();
-  const [supabaseClient] = useState(() => {
-    if (!session) return null;
-    return createClerkSupabaseClient(() => session.getToken({ template: 'supabase' }));
-  });
+  const [image, setImage] = useState<string | null>(null);
+  const { user } = useUser();
 
   useEffect(() => {
-    if (!supabaseClient) return;
-    const fetchUserImage = async () => {
-      const { data, error } = await supabaseClient.auth.getSession()
-      if (error) {
-        console.error(error)
-      }
-
-      setImage(data.session?.user.user_metadata.avatar_url ?? null)
+    if (user?.imageUrl) {
+      setImage(user.imageUrl);
     }
-    fetchUserImage()
-  }, [])
+  }, [user]);
 
-  return image
-}
+  return image;
+};
